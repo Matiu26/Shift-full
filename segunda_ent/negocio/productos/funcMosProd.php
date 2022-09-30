ad5f6a105d574fa7e32510822310829da5a1f4bb
Matiu26 <matebarsa@hotmail.com>
2022-09-29 22:59:33 -0300

    commit

--- segunda_ent/negocio/productos/funcMosProd.php
+++ segunda_ent/negocio/productos/funcMosProd.php
@@ -9,7 +9,7 @@
 
     <table id="tabla" width="40%" border="1">
         <tr>
-            <th>Id Producto</th>
+            <th>Id Product0</th>
             <th>Nombre</th>
             <th>Stock</th>
             <th>Precio</th>
