d56d9e42967a5765c0f2ba8294d77894908e7e5f
Matiu26 <matebarsa@hotmail.com>
2022-11-06 12:04:45 -0300

    bug de descuento paq

--- segunda_ent/negocio/paquete/funcCrearPaquete.php
+++ segunda_ent/negocio/paquete/funcCrearPaquete.php
@@ -2,9 +2,9 @@
     require_once("miapp_paquete.php");
     if (isset($_POST['agregar'])) {
 
-        if (isset($_POST['nom'])  && isset($_POST['pre']) && isset($_POST['descu'])  && isset($_POST['desc'])) {
+        if (isset($_POST['nom'])  && isset($_POST['pre']) && isset($_POST['desc'])) {
 
-                if (empty($_POST['nom']) || empty($_POST['pre']) || empty($_POST['descu']) || empty($_POST['desc'])) {
+                if (empty($_POST['nom']) || empty($_POST['pre']) || empty($_POST['desc'])) {
 
                     return;
                 }
@@ -14,7 +14,9 @@
                 $descuento = $_POST['descu'];
                 $descripcion = $_POST['desc'];
                 
-
+                if($_POST['descu'] == null || empty($_POST['descu'])  ){    
+                    $descuento= "0";
+                }
 
                 if ( agregar_paq($nombre,$precio,$descuento,$descripcion) == true) {
                     echo '<script language="javascript">alert("Paquete creado correctamente");</script>';
